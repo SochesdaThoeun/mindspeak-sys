a2aca7b17b3349abf9cf6b22673d7587b189e3a4
jerryscd123 <32119825+jerryscd123@users.noreply.github.com>
2025-06-22 17:31:24 +0700

    Updated Next Config

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'localhost', 'www.gravatar.com'],
+    domains: ['images.unsplash.com', 'localhost', 'www.gravatar.com', 'api.mindspeak.xyz'],
   },
 }
 
